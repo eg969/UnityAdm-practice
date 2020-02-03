c673bec0209a803a4ff91e6ea75f94dde975e20d
eg969 <Edgars.grivcovs@bbc.co.uk>
2020-02-03 15:35:32 +0000

    Not working code

--- Unity/Assets/Test.cs
+++ Unity/Assets/Test.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using static AudioBlockWrapper;
 using System.Text;
 using System;
+using System.Threading;
 
 public class Test : MonoBehaviour
 {
@@ -18,36 +19,82 @@ public class Test : MonoBehaviour
     Vector3 start;
     Vector3 end;
 
+    Thread newThread;
+
+
+
+
     void Start()
+    {
+        newThread = new Thread(new ThreadStart(getBlocksThread));
+        newThread.Start();
+    }
+
+    void getBlocksThread()
     {
         int j = 0;
         while (queryBlock(j, 0))
         {
             List<UnityAudioBlock> blocks = new List<UnityAudioBlock>();
             int k = 0;
-            GameObject audioObjectInstance = Instantiate(objectInstance) as GameObject;
-            audioObjects.Add(audioObjectInstance);
+            //GameObject audioObjectInstance = Instantiate(objectInstance) as GameObject;
+            //audioObjects.Add(audioObjectInstance);
 
             while (queryBlock(j, k))
             {
                 UnityAudioBlock nextBlock = getBlock(j, k);
                 blocks.Add(nextBlock);
-                GameObject objectPosition = Instantiate(objectPositionInstance) as GameObject;
+                //GameObject objectPosition = Instantiate(objectPositionInstance) as GameObject;
 
-                objectPosition.transform.position = new Vector3(nextBlock.x, nextBlock.y, nextBlock.z);
+                //objectPosition.transform.position = new Vector3(nextBlock.x, nextBlock.y, nextBlock.z);
                 k++;
             }
 
             channelFormats.Add(blocks);
             j++;
         }
+    }
 
-
-
-
-
-        for (int y = 0; y <channelFormats.Count; y++)
+    void addAnimations()
+    {
+        initializeObjects();
+        for (int y = 0; y < channelFormats.Count; y++)
         {
+            GameObject audioObject = audioObjects[y];
+            Animation objectAnimation = audioObject.GetComponent<Animation>();
+            AnimationClip clip = objectAnimation.GetClip(Encoding.ASCII.GetString(channelFormats[y][0].name));
+
+            AnimationCurve xCurve = new AnimationCurve();
+            AnimationCurve yCurve = new AnimationCurve();
+            AnimationCurve zCurve = new AnimationCurve();
+
+            newThread.Abort();
+
+            for (int i = 0; i < channelFormats[y].Count; i++)
+            {
+                xCurve.AddKey(new Keyframe(channelFormats[y][i].rTime, channelFormats[y][i].x));
+                yCurve.AddKey(new Keyframe(channelFormats[y][i].rTime, channelFormats[y][i].y));
+                zCurve.AddKey(new Keyframe(channelFormats[y][i].rTime, channelFormats[y][i].z));
+
+                clip.SetCurve("", typeof(Transform), "localPosition.x", xCurve);
+                clip.SetCurve("", typeof(Transform), "localPosition.y", yCurve);
+                clip.SetCurve("", typeof(Transform), "localPosition.z", zCurve);
+
+                objectAnimation.PlayQueued(clip.name, QueueMode.PlayNow, PlayMode.StopAll);
+            }
+            /////
+            newThread.Start();
+        }   
+    }
+
+
+    void initializeObjects()
+    {
+        for (int y = 0; y < channelFormats.Count; y++)
+        {
+            GameObject audioObjectInstance = Instantiate(objectInstance) as GameObject;
+            audioObjects.Add(audioObjectInstance);
+
             GameObject audioObject = audioObjects[y];
             audioObject.AddComponent<Animation>();
 
@@ -56,58 +103,14 @@ public class Test : MonoBehaviour
             clip.name = Encoding.ASCII.GetString(channelFormats[y][0].name);
             clip.legacy = true;
 
-            Keyframe[] xKeys = new Keyframe[channelFormats[y].Count];
-            Keyframe[] yKeys = new Keyframe[channelFormats[y].Count];
-            Keyframe[] zKeys = new Keyframe[channelFormats[y].Count];
-
-            for (int i = 0; i < channelFormats[y].Count; i++)
-            {
-                xKeys[i] = new Keyframe(channelFormats[y][i].rTime, channelFormats[y][i].x);
-                yKeys[i] = new Keyframe(channelFormats[y][i].rTime, channelFormats[y][i].y);
-                zKeys[i] = new Keyframe(channelFormats[y][i].rTime, channelFormats[y][i].z);
-            }
-
-            AnimationCurve xCurve = new AnimationCurve(xKeys);
-            clip.SetCurve("", typeof(Transform), "localPosition.x", xCurve);
-
-            AnimationCurve yCurve = new AnimationCurve(yKeys);
-            clip.SetCurve("", typeof(Transform), "localPosition.y", yCurve);
-
-            AnimationCurve zCurve = new AnimationCurve(zKeys);
-            clip.SetCurve("", typeof(Transform), "localPosition.z", zCurve);
-
             objectAnimation.AddClip(clip, clip.name);
             objectAnimation.Play(clip.name, PlayMode.StopSameLayer);
-
-            xCurve.AddKey(new Keyframe(21.0f, 0.0f));
-            yCurve.AddKey(new Keyframe(21.0f, 10.0f));
-            zCurve.AddKey(new Keyframe(21.0f, 10.0f));
-
-            clip.SetCurve("", typeof(Transform), "localPosition.x", xCurve);
-            clip.SetCurve("", typeof(Transform), "localPosition.y", yCurve);
-            clip.SetCurve("", typeof(Transform), "localPosition.z", zCurve);
-
-            objectAnimation.PlayQueued(clip.name, QueueMode.PlayNow, PlayMode.StopAll);
-
-            xCurve.AddKey(new Keyframe(22.0f, 0.0f));
-            yCurve.AddKey(new Keyframe(22.0f, 0.0f));
-            zCurve.AddKey(new Keyframe(22.0f, 0.0f));
-
-            clip.SetCurve("", typeof(Transform), "localPosition.x", xCurve);
-            clip.SetCurve("", typeof(Transform), "localPosition.y", yCurve);
-            clip.SetCurve("", typeof(Transform), "localPosition.z", zCurve);
-
-            objectAnimation.PlayQueued(clip.name, QueueMode.PlayNow, PlayMode.StopAll);
-
-
-
         }
     }
 
-
     void Update()
     {
-        
+        addAnimations();
     }
 
 }
