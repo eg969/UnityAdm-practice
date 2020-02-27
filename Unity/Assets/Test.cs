ae37d6c267150a82d208de5a7980a0ebf300929d
eg969 <Edgars.grivcovs@bbc.co.uk>
2020-02-27 16:28:47 +0000

    Working but not rendering and inefficient loop

--- Unity/Assets/Test.cs
+++ Unity/Assets/Test.cs
@@ -10,6 +10,7 @@ using UnityEditor;
 public class Test : MonoBehaviour
 {
     Thread getBlocksThread;
+    Dictionary<int, GameObject> audioObjects = new Dictionary<int, GameObject>();
 
     void Start()
     {
@@ -23,39 +24,40 @@ public class Test : MonoBehaviour
         {
             foreach (var channelFormat in channelFormats)
             {
-                if (channelFormat.gameObject == null)
+                if (!audioObjects.ContainsKey(channelFormat.Value.cfId))
                 {
-                    channelFormat.gameObject = new GameObject(channelFormat.name);
+                    audioObjects.Add(channelFormat.Value.cfId, new GameObject(channelFormat.Value.name));
                 }
 
-                doPositionFor(channelFormat);
+                doPositionFor(channelFormat.Value.cfId);
             }
         }
     }
 
-    Vector3 doPositionFor(UnityAudioChannelFormat* channelFormat)
+    void doPositionFor(int cfId)
     {
         float timeSnapshot = Time.fixedTime;
 
-        while (channelFormat->currentAudioBlocksIndex < channelFormat->audioBlocks.Count)
+        while (channelFormats[cfId].currentAudioBlocksIndex < channelFormats[cfId].audioBlocks.Count)
         {
-            UnityAudioBlock* audioBlock = channelFormat->audioBlocks[channelFormat->currentAudioBlocksIndex];
-            if (audioBlock->startTime <= timeSnapshot)
+            UnityAudioBlock audioBlock = channelFormats[cfId].audioBlocks[channelFormats[cfId].currentAudioBlocksIndex];
+            if (audioBlock.startTime <= timeSnapshot)
             {
                 // This block has started. Has it ended?
-                if(audioBlock->endTime > timeSnapshot)
+                if(audioBlock.endTime > timeSnapshot)
                 {
                     // We are in the block - find the interpolant (progress in to interpolation ramp)
-                    float interpolant = (timeSnapshot - audioBlock->startTime) / audioBlock->duration;
-                    channelFormat->gameObject.transform.position = Vector3.Lerp(audioBlock->startPos, audioBlock->endPos, interpolant);
+                    float interpolant = (timeSnapshot - audioBlock.startTime) / audioBlock.duration;
+                    audioObjects[cfId].transform.position = Vector3.Lerp(audioBlock.startPos, audioBlock.endPos, interpolant);
                     break;
                 }
                 else
                 {
+                    Debug.Log(cfId + " " + channelFormats[cfId].currentAudioBlocksIndex);
                     // It has ended. Make sure we set the position to it's final resting place.
-                    channelFormat->gameObject.transform.position = audioBlock->endPosition;
+                    audioObjects[cfId].transform.position = audioBlock.endPos;
                     // Now increment currentAudioBlocksIndex and re-evaluate the while - we might have already started the next block.
-                    channelFormat->currentAudioBlocksIndex++;
+                    // TODO - can not modify from Dict - channelFormats[cfId].currentAudioBlocksIndex++;
                 }
 
             }
--- Unity/Assets/plugins/AudioBlockWrapper.cs
+++ Unity/Assets/plugins/AudioBlockWrapper.cs
@@ -41,7 +41,6 @@ public class AudioBlockWrapper
 
     public struct UnityAudioBlock
     {
-        public int cfId;
         public int blockId;
         public float startTime;
         public float endTime;
@@ -53,7 +52,7 @@ public class AudioBlockWrapper
     public struct UnityAudioChannelFormat
     {
         public string name;
-        public GameObject* gameObject;
+        public int cfId;
         public List<UnityAudioBlock> audioBlocks;
         public int currentAudioBlocksIndex;
     };
@@ -80,36 +79,38 @@ public class AudioBlockWrapper
                 // (it's an ADM stipulation anyway, although might not be the case for S-ADM depending upon the transport mechanism used.)
                 // We do simple checks to confirm this, and discard those blocks that don't continue on from existing blocks.
 
-                UnityAudioBlock newBlock;
-                newBlock.blockId = nextBlock.blockId;
-                newBlock.startTime = nextBlock.rTime;
-                newBlock.endTime = nextBlock.rTime + nextBlock.duration;
-                newBlock.duration = nextBlock.duration;
-                newBlock.startPos.Set(nextBlock.x, nextBlock.y, nextBlock.z); // Will be overwritten if we find a previous block, otherwise (if first block) this is correct
-                newBlock.endPos.Set(nextBlock.x, nextBlock.y, nextBlock.z);
+                UnityAudioBlock newBlock = new UnityAudioBlock {
+                    blockId = nextBlock.blockId,
+                    startTime = nextBlock.rTime,
+                    endTime = nextBlock.rTime + nextBlock.duration,
+                    duration = nextBlock.duration,
+                    startPos = new Vector3(nextBlock.x, nextBlock.y, nextBlock.z), // Will be overwritten if we find a previous block, otherwise (if first block) this is correct
+                    endPos = new Vector3(nextBlock.x, nextBlock.y, nextBlock.z)
+                };
 
                 if (!channelFormats.ContainsKey(nextBlock.cfId))
                 {
                     lock (lockObject)
                     {
+
                         channelFormats.Add(nextBlock.cfId,
-                            new UnityAudioChannelFormat(
-                                nextBlock.name,
-                                null,
-                                List<UnityAudioBlock>(),
-                                0
-                            )
+                            new UnityAudioChannelFormat {
+                                name = Encoding.ASCII.GetString(nextBlock.name),
+                                cfId = nextBlock.cfId,
+                                audioBlocks = new List<UnityAudioBlock>(),
+                                currentAudioBlocksIndex = 0
+                            }
                         );
                     }
                 }
 
                 if (channelFormats[nextBlock.cfId].audioBlocks.Count > 0) {
+                    UnityAudioBlock previousBlock = channelFormats[nextBlock.cfId].audioBlocks[channelFormats[nextBlock.cfId].audioBlocks.Count - 1];
                     lock (lockObject)
                     {
-                        UnityAudioBlock* previousBlock = channelFormats[nextBlock.cfId].audioBlocks[channelFormats[nextBlock.cfId].audioBlocks.Count - 1];
-                        if (newBlock.blockId > previousBlock->blockId && newBlock.startTime >= previousBlock->endTime)
+                        if (newBlock.blockId > previousBlock.blockId && newBlock.startTime >= previousBlock.endTime)
                         {
-                            newBlock.startPos.Set(previousBlock->endPos.x, previousBlock->endPos.y, previousBlock->endPos.z);
+                            newBlock.startPos.Set(previousBlock.endPos.x, previousBlock.endPos.y, previousBlock.endPos.z);
                             channelFormats[nextBlock.cfId].audioBlocks.Add(newBlock);
                         }
                     }
@@ -129,145 +130,4 @@ public class AudioBlockWrapper
 
     }
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-public struct UnityAudioBlock
-    {
-        public string name;
-        public int cfId;
-        public int blockId;
-        public float rTime;
-        public float x;
-        public float y;
-        public float z;
-    };
-
-
-    public class UnityAudioChannelFormat
-    {
-        public int cfId;
-        public Vector3 startPos;
-        public Vector3 endPos;
-        public bool moivng;
-        public float lerpTime;
-        public float currentLerpTime;
-        public List<UnityAudioBlock> audioBlocks;
-    }
-
-
-
-    public static List<UnityAudioChannelFormat> channelFormats = new List<UnityAudioChannelFormat>();
-
-    public static readonly object lockObject = new object();
-
-    public static void readFile()
-    {
-        readAdm();
-    }
-
-    public static void getNewBlock()
-    {
-        readFile();
-        while (true)
-        {
-            CAudioBlock nextBlock = getNextBlock();
-
-            if (nextBlock.newBlockFlag != 0)
-            {
-                UnityAudioBlock newBlock;
-                newBlock.name = Encoding.ASCII.GetString(nextBlock.name);
-                newBlock.cfId = nextBlock.cfId;
-                newBlock.blockId = nextBlock.blockId;
-                newBlock.rTime = nextBlock.rTime;
-                newBlock.x = nextBlock.x;
-                newBlock.y = nextBlock.y;
-                newBlock.z = nextBlock.z;
-
-
-                bool found = false;
-
-                for (int i = 0; i < channelFormats.Count; i++)
-                {
-                    if (channelFormats[i].cfId == newBlock.cfId)
-                    {
-                        lock (lockObject)
-                        {
-                            channelFormats[i].audioBlocks.Add(newBlock);
-                        }
-
-                        found = true;
-                    }
-                }
-                if (!found)
-                {
-                    UnityAudioChannelFormat newChannelFormat = new UnityAudioChannelFormat();
-                    newChannelFormat.cfId = newBlock.cfId;
-                    newChannelFormat.lerpTime = 0f;
-                    newChannelFormat.moivng = false;
-                    newChannelFormat.startPos = new Vector3();
-                    newChannelFormat.endPos = new Vector3();
-                    newChannelFormat.currentLerpTime = 0f;
-                    newChannelFormat.audioBlocks = new List<UnityAudioBlock>();
-                    newChannelFormat.audioBlocks.Add(newBlock);
-                    lock (lockObject)
-                    {
-                        channelFormats.Add(newChannelFormat);
-                    }
-
-                }
-            }
-
-            //Thread.Sleep(50);
-
-        }
-
-    }
-
-    public static long nanoTime()
-    {
-        long nano = 10000L * Stopwatch.GetTimestamp();
-        nano /= TimeSpan.TicksPerMillisecond;
-        nano *= 100L;
-        return nano;
-    }
 }
